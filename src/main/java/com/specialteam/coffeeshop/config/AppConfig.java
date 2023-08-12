e1617af073882408dabf933b94adbba6b534e0ab
pradana4648 <wakwaujaim@gmail.com>
2023-08-12 10:50:49 +0700

    redesign

--- src/main/java/com/specialteam/coffeeshop/config/AppConfig.java
+++ src/main/java/com/specialteam/coffeeshop/config/AppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * This is main configuration for this app
+ */
 @Configuration
 public class AppConfig {
     @Bean
--- src/main/java/com/specialteam/coffeeshop/util/AppUtils.java
+++
@@ -1,4 +0,0 @@
-package com.specialteam.coffeeshop.util;
-
-public class AppUtils {
-}
